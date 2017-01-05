09f0b56daa0968bd50b2c77cf3ba78b4ef04d156
Kyle Alwyn <kjalwyn@gmail.com>
2017-01-04 16:53:00 -0800

    install express

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express')
 
-const app = express()
+const app = express();
 const indexPath = `${__dirname}/build/index.html`;
 const publicPath = express.static(`${__dirname}/build/assets`);
 
-app.use('/assets', publicPath)
-app.get('/*', function (_, res) { res.sendFile(indexPath) })
+app.use('/assets', publicPath);
+app.get('/*', function (_, res) { res.sendFile(indexPath) });
 
-module.exports = app
+module.exports = app;
