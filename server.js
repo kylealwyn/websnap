36277f5cb34736311112a86a1a7ef9df9ac29bbd
Kyle Alwyn <kjalwyn@gmail.com>
2017-01-04 16:51:23 -0800

    fix typo

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const app = express()
 const indexPath = `${__dirname}/build/index.html`;
-const publicPath = express.static(`${__dirname}/build/assets`;
+const publicPath = express.static(`${__dirname}/build/assets`);
 
 app.use('/assets', publicPath)
 app.get('/*', function (_, res) { res.sendFile(indexPath) })
